4a227881581414139ca67231dd2032200a692782
Gerardo Javier Quispe Chavez <gquispch@emeal.nttdata.com>
2023-12-27 21:20:58 -0500

    avance

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,21 @@
+import FtLogo from "../assets/images/svg/ftgo_logo.svg";
+import BgLogin from "../assets/images/png/background-home.png";
+
 const LoginPage = () => {
-  return <div>principal</div>;
+  return (
+    <div className="p-5">
+      <img src={FtLogo} alt="Fit Jaguar Logo" width={100} />
+      <div className="relative">
+        <img
+          src={BgLogin}
+          alt={"background"}
+          className="w-full object-cover"
+          style={{ height: "auto" }}
+        />
+        <div>asd</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
